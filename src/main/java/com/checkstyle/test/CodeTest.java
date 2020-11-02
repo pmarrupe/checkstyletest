04896cc9ac1d1f17b9e06119709a3bac9d78e629
Prashanth Marrupedhi <prashanth@Prashanths-MacBook-Pro.local>
2020-11-02 10:30:11 -0500

    CheckStyle Repo

---
+++ src/main/java/com/checkstyle/test/CodeTest.java
@@ -0,0 +1,13 @@
+package com.checkstyle.test;
+
+import java.io.*;
+import java.util.*;
+
+public class CodeTest {
+
+    public void add(List<String> list) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new File(""));
+        Collections.reverse(list);
+    }
+
+}
